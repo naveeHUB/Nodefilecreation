01f3febce5d01a2cf22de9add64af3e842fc964d
naveeHUB <naveekrish309@gmail.com>
2023-11-16 15:58:19 +0530

    Successfully Node Date and Time file created

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ HTTP_SERVER.use(bodyParser.urlencoded({ extended: false }));
 const port = 5000;
 const localhost= '127.0.0.1'
 
-HTTP_SERVER.listen(port, 'localhost', () => {
+HTTP_SERVER.listen(port, localhost, () => {
   console.log(`SERVER STARTED IN - http://${localhost}:${port}`);
 });
 
